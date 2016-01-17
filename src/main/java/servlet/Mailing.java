b71c875f5abb922f9a2c0af0e49c57cf44f9fe30
javier.boga.rioja@gmail.com <javier.boga.rioja@gmail.com>
2016-01-17 12:38:24 +0100

    tryh

--- src/main/java/servlet/Mailing.java
+++ src/main/java/servlet/Mailing.java
@@ -1,11 +1,14 @@
 package servlet;
 
+import java.util.Properties;
+
 import javax.annotation.Resource;
 import javax.ejb.Asynchronous;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -23,9 +26,10 @@ public class Mailing {
  private static final Logger log = Logger.getLogger(Mailing.class);
  
  @Resource(mappedName="java:jboss/mail/Default")
+ //@Resource(mappedName="java:jboss/mail/Gmail")
  Session gmailSession;
- 
- /**
+ Properties props = new Properties();
+  /**
      * Default constructor. 
      */
     public Mailing() {
@@ -35,23 +39,33 @@ public class Mailing {
     public void sendEmail(String to, String from, String subject, String content) {
      
      log.info("Sending Email from " + from + " to " + to + " : " + subject);
-     
-  try {
+     props.put("mail.smtp.host", "smtp.gmail.com");
+     props.put("mail.smtp.socketFactory.port", "465");
+	 props.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
+	 props.put("mail.smtp.auth", "true");
+	 props.put("mail.smtp.port", "465");
+
+	 try {
+	 /* gmailSession = Session.getDefaultInstance(props,new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication("javier.boga.rioja@gmail.com","limitado10");
+					}
+	  });*/
+
+     Message message = new MimeMessage(gmailSession);
+     message.setFrom(new InternetAddress(from));
+     message.setRecipients(Message.RecipientType.TO,
+     InternetAddress.parse(to));
+     message.setSubject(subject);
+     message.setText(content);
  
-   Message message = new MimeMessage(gmailSession);
-   message.setFrom(new InternetAddress(from));
-   message.setRecipients(Message.RecipientType.TO,
-    InternetAddress.parse(to));
-   message.setSubject(subject);
-   message.setText(content);
+     Transport.send(message);
+  
+     log.info("Email was sent");
  
-   Transport.send(message);
- 
-   log.debug("Email was sent");
- 
-  } catch (MessagingException e) {
-   log.error("Error while sending email : " + e.getMessage());
-  }
+	 } catch (Exception e) {
+		 log.error("Error while sending email : " + e.getMessage());
+	 }
     }
 
 }
